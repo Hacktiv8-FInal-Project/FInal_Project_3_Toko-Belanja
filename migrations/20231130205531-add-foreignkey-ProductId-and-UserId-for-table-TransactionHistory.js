50d2fa0b2743b77125e115cc16b25264c32a9945
danirizkyzaelani <danirizky704@gmail.com>
2023-12-02 07:57:31 +0700

    fix: foreignkey category and product

--- migrations/20231130205531-add-foreignkey-ProductId-and-UserId-for-table-TransactionHistory.js
+++ migrations/20231130205531-add-foreignkey-ProductId-and-UserId-for-table-TransactionHistory.js
@@ -9,6 +9,35 @@ module.exports = {
      * Example:
      * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
      */
+    await queryInterface.addColumn('TransactionHistories', 'ProductId', {
+      type: Sequelize.INTEGER,
+    })
+    await queryInterface.addConstraint('TransactionHistories', {
+      fields: ['ProductId'],
+      type: 'foreign key',
+      name: 'fk_TransactionHistories_ProductId',
+      references: {
+        table: 'Products',
+        field: 'id'
+      },
+      onDelete: 'cascade',
+      onUpdate: 'cascade'
+    })
+
+    await queryInterface.addColumn('TransactionHistories', 'UserId', {
+      type: Sequelize.INTEGER,
+    })
+    await queryInterface.addConstraint('TransactionHistories', {
+      fields: ['UserId'],
+      type: 'foreign key',
+      name: 'fk_TransactionHistories_UserId',
+      references: {
+        table: 'Users',
+        field: 'id'
+      },
+      onDelete: 'cascade',
+      onUpdate: 'cascade'
+    })
   },
 
   async down (queryInterface, Sequelize) {
@@ -18,5 +47,9 @@ module.exports = {
      * Example:
      * await queryInterface.dropTable('users');
      */
+    await queryInterface.removeColumn('TransactionHistories', 'ProductId')
+    await queryInterface.removeConstraint('TransactionHistories', 'fk_TransactionHistories_ProductId')
+    await queryInterface.removeColumn('TransactionHistories', 'UserId')
+    await queryInterface.removeConstraint('TransactionHistories', 'fk_TransactionHistories_UserId')
   }
 };
